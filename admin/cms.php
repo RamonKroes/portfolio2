a82b1a59586f408fb0f36bf49fb99f6a2f746338
RamonKroes <40887343+RamonKroes@users.noreply.github.com>
2018-10-31 18:33:56 +0100

    So much added

---
+++ admin/cms.php
@@ -0,0 +1,14 @@
+<?php
+include('connect.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CMS</title>
+</head>
+<body>
+	Welcome <?= $username ?>
+</body> 
+</html>
--- sub-sites/portfolio.php
+++ sub-sites/portfolio.php
@@ -4,16 +4,16 @@
 			<h2>ENKELE <span>PORTFOLIO</span> ITEMS</h2>
 		</div>
 		<div class="portfolio-item port-left">
-			<img src="img/s27logo.jpg">
+			<img src="img/s27logo.png">
 		</div>
 		<div class="portfolio-item port-left">
-			<img src="img/s27logo.jpg">
+			<img src="img/s27logo.png">
 		</div>
 		<div class="portfolio-item port-right">
-			<img src="img/s27logo.jpg">
+			<img src="img/s27logo.png">
 		</div>
 		<div class="portfolio-item port-right">
-			<img src="img/s27logo.jpg">
+			<img src="img/s27logo.png">
 		</div>
 	</div>
 </main>
